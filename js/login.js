af8dfe9516e65bef2905beb8c3ffb5851586c931
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-11-11 20:44:25 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,18 @@
+correctPWD="abd124"
+pwd;
 function lock(){
     window.location = 'lock.html'
 }
 function github(){
     window.location= 'https://github.com/Pandademic/mac2'
+}
+function login(){
+    pwd=document.getElementById('pwd-box').value;
+    if(pwd=='abd124'){
+        window.location="desktop.html"
+    }
+    else{
+        console.error("incorrect password");
+        window.location="lock.html";
+    }
 }
