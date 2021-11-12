b1bb728c809c57bd8cd95aac024443894590a80d
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-11-11 20:34:31 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,6 +2,8 @@ function lock(){
     window.location = 'lock.html'
 }
 function shutdown(){
+   console.log("shutting down in 5 seconds");
+   setTimeout(5000);
    document.getElementById('app').style.animation = "fade";
    
 }
