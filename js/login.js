f26980532a08101db7507ce92a70ed2bb76fb51f
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-11-11 20:37:35 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,6 @@
 function lock(){
     window.location = 'lock.html'
 }
-function shutdown(){
-   console.log("shutting down in 5 seconds");
-   setTimeout(5000);
-   document.getElementById('app').style.animation = "fade";
-   
-}
 function github(){
     window.location= 'https://github.com/Pandademic/mac2'
 }
